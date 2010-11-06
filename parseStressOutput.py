bd7874b20e9a59d4e443381263f5a970a1e89ad3
Jake <jake@evansturner.co.uk>
2010-11-06 12:00:27 +0000

    WIP - trying parse callstack robustly for PC/360/PS3 - need to parse callstack entry number

--- parseStressOutput.py
+++ parseStressOutput.py
@@ -1,4 +1,4 @@
-import fileinput
+import string
 
 # Callstack example inside a big log file
 # 04/11/2010 13:10:21.11 Perf_cw2_church_04_11_2010__12_56:RUN 
@@ -15,12 +15,16 @@ parseCALLSTACK = 1
 state = lookingForRUN
 callstackState = lookingForSTART
 
-inputfile = file("/home/jake/Documents/main_pc.log", "rU")
+# inputfile = file("/home/jake/Documents/main_pc.log", "rU")
+# inputfile = file("/home/jake/Documents/main_360.log", "rU")
+inputfile = file("/home/jake/Documents/main_ps3.log", "rU")
 line = inputfile.readline()
 
 while len(line):
 	# remove the newline from the line string
 	line = line.splitlines()[0]
+	line = line.strip()
+
 	if state == lookingForRUN:
 		# Look for the RUN line e.g. 04/11/2010 13:10:21.11 Perf_cw2_church_04_11_2010__12_56:RUN 
 		if line.endswith(":RUN"):
@@ -60,9 +64,21 @@ while len(line):
 			# Now looking for the start of the call stack e.g. "Call Stack Trace:"
 			if line.startswith("Call Stack Trace:"):
 				callstackState = parseCALLSTACK
-		elif callstackState == parseCALLSTACK:
+				continue
+		if callstackState == parseCALLSTACK:
 			# Parse each stack line
+		  	# Looking for patterns like this: 
+		  	# 38) function=0x8009C4B4
+		  	# 32) IDebugCallStack::FatalError() [idebugcallstack.cpp:144] 
+		  	# i.e. <number> + ") " + "function=" + <ASCII>
+		  	# OR
+		  	# i.e. <number> + ") " + <ASCII> + " [" + <ASCII> + ":" + <number> + "]"
+		  	lineTokens = line.split(")")
+			if unicode(lineTokens[0]).isnumeric() == False:
+				continue
 
+			entryNumber = int(lineTokens[0])
+			print "Number = " + str(entryNumber)
 			if len(line) > 0:
 				currentCallstack.append(line)
 
